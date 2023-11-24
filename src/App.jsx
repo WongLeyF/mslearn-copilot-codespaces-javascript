9940eeb0726e980764f3cba865daaa8d01683c82
WongLeyF <76757295+WongLeyF@users.noreply.github.com>
2023-11-24 04:50:34 +0000

    feat: new animations to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Juan Manuel Wong Ley Franco",
+  title: "Full Stack Developer",
+  email: "jmanuelwlf@hotmail.com",
+  gitHub: "WongLeyF",
+  instagram: "WongLeyF",
+  linkedIn: "WongLeyF",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Mrkemey",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
